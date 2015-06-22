018f2ecdb56c05bd17810dd2b8a58704a7ce99c0
hooperk <hooperk@tcd.ie>
2015-06-22 15:03:38 +0100

    Entry struct made, programmatic booking plan needed

---
+++ Assignment Fiddling/Entry.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Text;
+
+namespace Assignment_Fiddling
+{
+    struct Entry
+    {
+        String Name { get; set; }
+        DateTime Date { get; set; }
+        String Price { get; set; }
+        Boolean[] Booked { get; set; }
+
+        public Entry(String name, String date, String price, String booked)
+        {
+            Name = name.Replace("\"","");
+            Date = DateTime.Parse(date.Replace("\"", ""));
+            Price = price.Replace("\"", "");
+            Booked = new Boolean[12];
+            Char[] seats = booked.Replace("\"", "").ToCharArray();
+            for (int i = 0; i < 12 && i < seats.Length; i++)
+                Booked[i] = (seats[i] == 'B');
+        }
+
+        public String GetDate()
+        {
+            return Date.ToString("d");
+        }
+
+        public override String ToString(){
+            StringBuilder output = new StringBuilder("\"" + Name + "\"" + Environment.NewLine + "\"" + GetDate() + "\""
+                + Environment.NewLine + "\"" + Price + "\"" + Environment.NewLine + "\"");
+            foreach (Boolean b in Booked)
+                output.Append(b ? "B" : "F");
+            output.Append("\"");
+            return output.ToString();
+        }
+    }
+}
--- Assignment Fiddling/Form1.Designer.cs
+++ Assignment Fiddling/Form1.Designer.cs
@@ -48,6 +48,11 @@
             // numericUpDown1
             // 
             this.numericUpDown1.Location = new System.Drawing.Point(189, 41);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
             this.numericUpDown1.Name = "numericUpDown1";
             this.numericUpDown1.Size = new System.Drawing.Size(120, 20);
             this.numericUpDown1.TabIndex = 1;
