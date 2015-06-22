fcaf8926b305c43d8dbe5166a2f83b5e389401a7
hooperk <hooperk@tcd.ie>
2015-06-22 10:51:16 +0100

    Lesson 3

---
+++ Variables and Datatypes/Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Variables_and_Datatypes
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int firstNumber;
+            int secondNumber;
+            int answer;
+
+            firstNumber = int.Parse(textBox1.Text);
+            secondNumber = int.Parse(textBox2.Text);
+
+            answer = firstNumber + secondNumber;
+            label3.Text = answer.ToString();
+        }
+    }
+}
